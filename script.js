ee1cd4346eefabeef2f74d3bde65c96cc32d3ec3
Melody4200244 <56429596+Melody4200244@users.noreply.github.com>
2024-07-30 15:30:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function confirmDate() {
     localStorage.setItem('location', location);
 
     // Send data to the server
-    fetch(' https://real-guests-try.loca.lt', { // Update this URL if deploying the server
+    fetch('https://red-gifts-admire.loca.lt', { // Update this URL if deploying the server
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
